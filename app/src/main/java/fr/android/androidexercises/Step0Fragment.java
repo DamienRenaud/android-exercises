64a2f96e15d5d89de24f1fdf95c375f68b21e0a8
Damien Renaud <renauddamien86@gmail.com>
2017-11-10 11:20:47 +0100

    Exercice sur les fragments

--- app/src/main/java/fr/android/androidexercises/Step0Fragment.java
+++ app/src/main/java/fr/android/androidexercises/Step0Fragment.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Step0Fragment extends Fragment {
@@ -20,6 +22,7 @@ public class Step0Fragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         // TODO cast context to listener
+        listener = (OnNextStep0Listener)context;
     }
 
     @Nullable
@@ -27,11 +30,14 @@ public class Step0Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_step0, container, false);
         // TODO findViewById textView (TextView)
+        final TextView textView = view.findViewById(R.id.textView);
         // TODO findViewById nextButton (Button)
+        Button nextButton = view.findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO call onNext() from listener
+                listener.onNext();
             }
         });
         return view;
@@ -41,11 +47,13 @@ public class Step0Fragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // TODO setText(step0)
+        ((TextView)view.findViewById(R.id.textView)).setText("Fragment 0 !");
     }
 
     public interface OnNextStep0Listener {
 
         // TODO add onNext() method
+        void onNext();
 
     }
 }
